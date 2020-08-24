342f8865ea5e3a85c404851b2779014b190a3295
Mateusz Sliwczynski <mateusz.sliwczynski@gmail.com>
2020-08-24 16:32:42 +0200

    make difficulty value dynamic

--- dev-test.js
+++ dev-test.js
@@ -1,5 +1,7 @@
-const Block = require('./bloblock');
+const Blockchain = require('./blockchain/index')
 
-const fooBlock = Block.mineBlock(Block.genesis(), 'foo');
+const bc = new Blockchain();
 
-console.log(fooBlock.toString());
+for (let i = 0; i < 10; i++) {
+  console.log(bc.addBlock(`foo ${i}`).toString());
+}
