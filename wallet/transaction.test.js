b96810b222e386686f6123dded082f0cbda49b6d
Mateusz Sliwczynski <mateusz.sliwczynski@gmail.com>
2020-08-25 11:56:38 +0200

    create transaction class with unit tests

---
+++ wallet/transaction.test.js
@@ -0,0 +1,35 @@
+const Transaction = require('./transaction');
+const Wallet = require('./index');
+
+describe('Transaction', () => {
+  let transaction, wallet, recipient, amount;
+  beforeEach(() => {
+    wallet = new Wallet();
+    amount = 50;
+    recipient = 'r4cwq31tn';
+    transaction = Transaction.newTransaction(wallet, recipient, amount);
+    console.log(transaction);
+
+  });
+
+  it('outputs the `amount` substracted from the wallet balance', () => {
+    expect(transaction.outputs.find(output => output.address === wallet.publicKey).amount)
+      .toEqual(wallet.balance - amount);
+  })
+
+  it('outputs the `amount` added to the recipient', () => {
+    expect(transaction.outputs.find(output => output.address === recipient).amount)
+      .toEqual(amount);
+  })
+
+  describe('transacting with an amount that exceeds the balance', () => {
+    beforeEach(() => {
+      amount = 5000;
+      transaction = Transaction.newTransaction(wallet, recipient, amount);
+    })
+
+    it('does not create the transaction', () => {
+      expect(transaction).toEqual(undefined);
+    })
+  })
+})
