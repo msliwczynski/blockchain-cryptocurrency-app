a6340286dc92efe476038b1f7774f0c9cceee40d
Mateusz Sliwczynski <mateusz.sliwczynski@gmail.com>
2020-08-25 11:29:54 +0200

    add util method for generating id

--- chain-util.js
+++ chain-util.js
@@ -1,9 +1,14 @@
 const EC = require('elliptic').ec;
+const uuidV1 = require('uuid/v1');
 const ec = new EC('secp256k1'); // Standards for Efficient Cryptography
 
 class ChainUtil {
   static genKeyPair() {
     return ec.genKeyPair();
+  }
+
+  static id() {
+    return uuidV1();
   }
 
 }
